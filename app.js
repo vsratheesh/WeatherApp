f81f8af8112565d556d9e5a4f76e188e03333484
asifaly <asifaly82@yahoo.com>
2016-03-29 10:32:22 +0530

    forecaset at diff times for a day added

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ $(document).ready(function () {
     var weatherStr = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=bc1b6968884b3366859f9679983dc947';
     $.ajax({
       url: weatherStr,
-      error: function () {
-      },
+      error: function () {},
       success: function (data) {
         var date = new Date(data.dt * 1000);
         var dateStr = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
@@ -31,27 +30,26 @@ $(document).ready(function () {
     var forecastStr = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=bc1b6968884b3366859f9679983dc947';
     $.ajax({
       url: forecastStr,
-      error: function () {
-      },
+      error: function () {},
       success: function (data) {
-        var filtered = data.list.filter(function (v, i, a) {
-          while (i >= 1) {
-            date1 = new Date(v.dt * 1000).getDate();
-            date2 = new Date(a[i - 1].dt * 1000).getDate();
-            return date1 !== date2 ? v : "";
-          }
-        });
-        var innerHtml="";
+        // var filtered = data.list.filter(function (v, i, a) {
+        //   while (i >= 1) {
+        //     date1 = new Date(v.dt * 1000).getDate();
+        //     date2 = new Date(a[i - 1].dt * 1000).getDate();
+        //     return date1 !== date2 ? v : "";
+        //   }
+        // });
+        var innerHtml = "";
         $('#textInfo').text('Forecast for 5 days for city ' + cityName + ',' + data.city.country);
-        filtered.forEach(function(e){
+        data.list.forEach(function (e) {
           var date = new Date(e.dt * 1000);
           var dateStr = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
+          var timeStr = date.getHours() + ':' + date.getMinutes();
           var temparature = Math.round(e.main.temp - 273.5, 2);
           var humidity = e.main.humidity;
           var weather = e.weather[0].description;
-          innerHtml += '<tr><td>' + dateStr + '</td><td>' + temparature + '</td><td>' + humidity + '</td><td>' + weather + '</td></tr>';
+          innerHtml += '<tr><td>' + dateStr + ' - ' + timeStr + '</td><td>' + temparature + '</td><td>' + humidity + '</td><td>' + weather + '</td></tr>';
         });
-
         $('tbody').append(innerHtml);
         $('#result').show();
       },
